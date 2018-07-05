a5201805d4fffe5fab4f3e9421cb9eef522a3c62
x li <atlas_li@126.com>
2018-07-06 00:14:28 +0800

    modify build sh

--- ESP8266_RTOS_SDK-2.0.0/app/project_template/gen_misc.sh
+++ ESP8266_RTOS_SDK-2.0.0/app/project_template/gen_misc.sh
@@ -8,8 +8,8 @@ export SDK_PATH=~/esp_iot_sdk_freertos
 export BIN_PATH=~/esp8266_bin
 !
 
-export SDK_PATH=$SDK_PATH
-export BIN_PATH=$BIN_PATH
+export SDK_PATH=/home/esp8266/Share/work/ESP8266/ESP8266_RTOS_SDK-2.0.0
+export BIN_PATH=$PWD/bin_out
 
 echo "gen_misc.sh version 20150911"
 echo ""

0b8edaaf801a4c27e68b98eeef1f94a27dee03aa
atlas1988 <atlas_li@126.com>
2018-07-06 18:01:27 +0800

    add down load tools

---
+++ tools/path_esp8266.sh
@@ -0,0 +1,4 @@
+export SDK_PATH=/home/esp8266/Share/work/ESP8266_RTOS_SDK
+export IDF_PATH=/home/esp8266/Share/work/ESP8266_RTOS_SDK
+export PATH=/home/esp8266/Share/xtensa-lx106-elf/bin:$PATH
+export BIN_PATH=/home/esp8266/Share/esp8266_bin
